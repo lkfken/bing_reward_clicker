6542821063fae30f7124f21d4097f3859657573d
Kenneth Leung <kenneth@leungs.us>
2019-04-29 15:03:25 -0700

    change from navigate.to to jump_to

--- lib/bing/login.rb
+++ lib/bing/login.rb
@@ -10,7 +10,7 @@ module Bing
     end
 
     def run
-      @browser.navigate.to URL
+      @browser.jump_to URL
       @browser.wait_for(10) {@browser.find_element(:name => 'loginfmt')}
       @browser.find_element(:name => 'loginfmt').send_key(@username)
       @browser.find_element(:name => 'loginfmt').send_key(Selenium::WebDriver::Keys[:return])
--- lib/bing/points.rb
+++ lib/bing/points.rb
@@ -18,7 +18,7 @@ module Bing
     end
 
     def points_detail
-      @browser.navigate.to POINTS_DETAIL_URL
+      @browser.jump_to POINTS_DETAIL_URL
       buckets = @browser.wait_for(10) {@browser.find_elements(:xpath => "//p[contains(@class, 'pointsDetail c-subheading-3 ng-binding')]")}
       edge_bonus, pc_search, mobile_search = buckets.map(&:text)
       {edge_bonus: edge_bonus, pc_search: pc_search, mobile_search: mobile_search}
