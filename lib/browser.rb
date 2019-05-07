142984fac96f749d6bfd3a6e3f9ae49d42d168f6
Kenneth Leung <kenneth@leungs.us>
2019-05-07 11:43:44 -0700

    changed mobile agent string

--- lib/browser.rb
+++ lib/browser.rb
@@ -6,7 +6,7 @@ class Browser
   class InvalidModeError < StandardError;
   end
 
-  MOBILE_AGENT = 'Mozilla/5.0 (Windows Phone 10.0; Android 6.0.0; WebView/3.0) ' + 'AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/64.118.222 ' + 'Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15063'
+  MOBILE_AGENT = 'Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; WebView/3.0) ' + 'AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/64.118.222 ' + 'Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15063'
   PC_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' + 'AppleWebKit/537.36 (KHTML, like Gecko) ' + 'Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134'
   VALID_MODES = [:pc, :mobile]
 
